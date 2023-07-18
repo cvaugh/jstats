15c182800defb1016f273af93a1c63991885508c
Chase Vaughan <git@cvaugh.dev>
2023-07-17 23:47:27 -0400

    Remove unnecessary null checks in LogEntry

--- src/main/java/dev/cvaugh/jstats/LogEntry.java
+++ src/main/java/dev/cvaugh/jstats/LogEntry.java
@@ -47,185 +47,59 @@ public class LogEntry implements Comparable<LogEntry> {
     public void process() {
         for(LogElement e : LogElement.values()) {
             String v = elements.get(e);
+            if(v == null)
+                return;
             switch(e) {
-            case CLIENT_IP -> {
-                if(v != null) {
-                    clientIP = v;
-                }
-            }
-            case UNDERLYING_PEER_IP -> {
-                if(v != null) {
-                    underlyingPeerIP = v;
-                }
-            }
-            case LOCAL_IP -> {
-                if(v != null) {
-                    localIP = v;
-                }
-            }
-            case SIZE_OF_RESPONSE -> {
-                if(v != null) {
-                    sizeOfResponse = Long.parseLong(v);
-                }
-            }
-            case SIZE_OF_RESPONSE_CLF -> {
-                if(v != null) {
-                    sizeOfResponse = v.equals("-") ? 0L : Long.parseLong(v);
-                }
-            }
-            case TIME_TO_SERVE_US -> {
-                if(v != null) {
-                    timeToServeUs = Long.parseLong(v);
-                }
-            }
-            case FILENAME -> {
-                if(v != null) {
-                    filename = v;
-                }
-            }
-            case REMOTE_HOSTNAME -> {
-                if(v != null) {
-                    remoteHostname = v;
-                }
-            }
-            case REMOTE_HOSTNAME_UNDERLYING -> {
-                if(v != null) {
-                    remoteHostnameUnderlying = v;
-                }
-            }
-            case REQUEST_PROTOCOL -> {
-                if(v != null) {
-                    requestProtocol = v;
-                }
-            }
-            case NUM_KEEPALIVE -> {
-                if(v != null) {
-                    numKeepalive = Integer.parseInt(v);
-                }
-            }
-            case REMOTE_LOGNAME -> {
-                if(v != null) {
-                    remoteLogname = v;
-                }
-            }
-            case REQUEST_ERROR_LOG_ID -> {
-                if(v != null) {
-                    requestErrorLogID = v;
-                }
-            }
-            case REQUEST_METHOD -> {
-                if(v != null) {
-                    requestMethod = v;
-                }
-            }
-            case PORT -> {
-                if(v != null) {
-                    port = Integer.parseInt(v);
-                }
-            }
-            case PID -> {
-                if(v != null) {
-                    pid = Integer.parseInt(v);
-                }
-            }
-            case QUERY -> {
-                if(v != null) {
-                    query = v;
-                }
-            }
-            case FIRST_LINE -> {
-                if(v != null) {
-                    firstLine = v;
-                }
-            }
-            case HANDLER -> {
-                if(v != null) {
-                    handler = v;
-                }
-            }
-            case STATUS -> {
-                if(v != null) {
-                    status = Integer.parseInt(v);
-                }
-            }
-            case STATUS_FINAL -> {
-                if(v != null) {
-                    statusFinal = Integer.parseInt(v);
-                }
-            }
+            case CLIENT_IP -> clientIP = v;
+            case UNDERLYING_PEER_IP -> underlyingPeerIP = v;
+            case LOCAL_IP -> localIP = v;
+            case SIZE_OF_RESPONSE -> sizeOfResponse = Long.parseLong(v);
+            case SIZE_OF_RESPONSE_CLF -> sizeOfResponse = v.equals("-") ? 0L : Long.parseLong(v);
+            case TIME_TO_SERVE_US -> timeToServeUs = Long.parseLong(v);
+            case FILENAME -> filename = v;
+            case REMOTE_HOSTNAME -> remoteHostname = v;
+            case REMOTE_HOSTNAME_UNDERLYING -> remoteHostnameUnderlying = v;
+            case REQUEST_PROTOCOL -> requestProtocol = v;
+            case NUM_KEEPALIVE -> numKeepalive = Integer.parseInt(v);
+            case REMOTE_LOGNAME -> remoteLogname = v;
+            case REQUEST_ERROR_LOG_ID -> requestErrorLogID = v;
+            case REQUEST_METHOD -> requestMethod = v;
+            case PORT -> port = Integer.parseInt(v);
+            case PID -> pid = Integer.parseInt(v);
+            case QUERY -> query = v;
+            case FIRST_LINE -> firstLine = v;
+            case HANDLER -> handler = v;
+            case STATUS -> status = Integer.parseInt(v);
+            case STATUS_FINAL -> statusFinal = Integer.parseInt(v);
             case TIME -> {
-                if(v != null) {
-                    Date date;
-                    String stripped = v.substring(1, v.length() - 1);
-                    try {
-                        date = Config.getInputDateFormat().parse(stripped);
-                    } catch(ParseException ex) {
-                        Logger.log("Failed to parse date: %s", Logger.WARN, stripped);
-                        continue;
-                    }
-                    time = date.getTime();
+                Date date;
+                String stripped = v.substring(1, v.length() - 1);
+                try {
+                    date = Config.getInputDateFormat().parse(stripped);
+                } catch(ParseException ex) {
+                    Logger.log("Failed to parse date: %s", Logger.WARN, stripped);
+                    continue;
                 }
+                time = date.getTime();
             }
-            case TIME_TO_SERVE_S -> {
-                if(v != null) {
-                    timeToServeS = Integer.parseInt(v);
-                }
-            }
-            case REMOTE_USER -> {
-                if(v != null) {
-                    remoteUser = v;
-                }
-            }
-            case URL -> {
-                if(v != null) {
-                    url = v;
-                }
-            }
-            case SERVER_NAME -> {
-                if(v != null) {
-                    serverName = v;
-                }
-            }
-            case SERVER_NAME_UCN -> {
-                if(v != null) {
-                    serverNameUCN = v;
-                }
-            }
+            case TIME_TO_SERVE_S -> timeToServeS = Integer.parseInt(v);
+            case REMOTE_USER -> remoteUser = v;
+            case URL -> url = v;
+            case SERVER_NAME -> serverName = v;
+            case SERVER_NAME_UCN -> serverNameUCN = v;
             case CONNECTION_STATUS -> {
-                if(v != null) {
-                    switch(v.charAt(0)) {
-                    case 'X' -> connectionStatus = ConnectionStatus.ABORTED;
-                    case '+' -> connectionStatus = ConnectionStatus.KEPT_ALIVE;
-                    case '-' -> connectionStatus = ConnectionStatus.CLOSED;
-                    default -> connectionStatus = ConnectionStatus.UNSET;
-                    }
+                switch(v.charAt(0)) {
+                case 'X' -> connectionStatus = ConnectionStatus.ABORTED;
+                case '+' -> connectionStatus = ConnectionStatus.KEPT_ALIVE;
+                case '-' -> connectionStatus = ConnectionStatus.CLOSED;
+                default -> connectionStatus = ConnectionStatus.UNSET;
                 }
             }
-            case BYTES_RECEIVED -> {
-                if(v != null) {
-                    bytesReceived = Long.parseLong(v);
-                }
-            }
-            case BYTES_SENT -> {
-                if(v != null) {
-                    bytesSent = Long.parseLong(v);
-                }
-            }
-            case BYTES_TRANSFERRED -> {
-                if(v != null) {
-                    bytesTransferred = Long.parseLong(v);
-                }
-            }
-            case REFERER -> {
-                if(v != null) {
-                    referer = v;
-                }
-            }
-            case USER_AGENT -> {
-                if(v != null) {
-                    userAgent = v;
-                }
-            }
+            case BYTES_RECEIVED -> bytesReceived = Long.parseLong(v);
+            case BYTES_SENT -> bytesSent = Long.parseLong(v);
+            case BYTES_TRANSFERRED -> bytesTransferred = Long.parseLong(v);
+            case REFERER -> referer = v;
+            case USER_AGENT -> userAgent = v;
             }
         }
     }
